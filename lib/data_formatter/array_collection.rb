8f76c4467b0636617904235e41a21424c040e980
Benjamin Curtis <benjamin.curtis@gmail.com>
2016-08-26 12:56:24 -0700

    Set an arbitrary limit of how many items of an array to render

--- lib/data_formatter/array_collection.rb
+++ lib/data_formatter/array_collection.rb
@@ -1,5 +1,6 @@
 module DataFormatter
   class ArrayCollection < Collection
+    Limit = 1000
 
     def open
       @open ||= Tag.new(css_class: "bracket", content: "[")
@@ -10,7 +11,7 @@ module DataFormatter
     end
 
     def prepare(data)
-      data.map { |v| Value.new(data: v, indentation: indentation, lang: lang) }
+      data.first(Limit).map { |v| Value.new(data: v, indentation: indentation, lang: lang) }
     end
 
   end
