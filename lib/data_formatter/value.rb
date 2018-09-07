6e097efe1c3cd67768baa4f1a8e68bff3b0eb34e
Benjamin Curtis <benjamin.curtis@gmail.com>
2018-09-07 11:46:32 -0700

    Replace unicode empty character

--- lib/data_formatter/value.rb
+++ lib/data_formatter/value.rb
@@ -44,7 +44,7 @@ module DataFormatter
       end
 
       def format_string
-        escaped_value = { "\a" => '\a', "\b" => '\b', "\r" => '\r', "\n" => '\n', "\t" => '\t' }.inject(value) do |memo, pair|
+        escaped_value = { "\a" => '\a', "\b" => '\b', "\r" => '\r', "\n" => '\n', "\t" => '\t', "\u0000" => '\u0000' }.inject(value) do |memo, pair|
           memo.gsub(pair[0], pair[1])
         end
         mark_up(data: escaped_value, surround: '"', kind: "string" )
